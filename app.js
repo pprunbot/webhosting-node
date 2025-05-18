a4e4876310921cf82749bd745e219f4f64c86812
pprunbot <freehotfans@protonmail.com>
2025-05-18 23:24:51 +0800

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nezha.gvkoyeb.eu.org';
 const NEZHA_PORT = process.env.NEZHA_PORT || '443';        // 端口为443时自动开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '';             // 哪吒三个变量不全不运行
 const DOMAIN = process.env.DOMAIN || '';  //项目域名或已反代的域名，不带前缀，建议填已反代的域名
-const NAME = process.env.NAME || 'VN-webhosting';
+const NAME = process.env.NAME || 'Webhosting-Node';
 const port = process.env.PORT || 4000;
 
 // 创建HTTP路由
